bb04aeeeba0d894a7311fb37a55d1e2bec73d595
Alexei Margasov <amargasov@naumen.ru>
2014-09-02 18:36:34 +0600

    fix create execution bug

--- app/controllers/impasse_execution_bugs_controller.rb
+++ app/controllers/impasse_execution_bugs_controller.rb
@@ -4,14 +4,14 @@ class ImpasseExecutionBugsController < ApplicationController
   menu_item :impasse
   
   def create_bug
-    execution_bug = Impasse::ExecutionBug.new(:execution_id => params[:execution_bug][:execution_id], :bug_id => params[:execution_bug][:issue_id])
+    execution_bug = Impasse::ExecutionBug.find_or_initialize_by_execution_id_and_bug_id(:execution_id => params[:execution_bug][:execution_id], :bug_id => params[:execution_bug][:issue_id])
     if execution_bug.save!
       respond_to do |format|
         format.json  { render :json => { :status => 'success' } }
       end
     else
       respond_to do |format|
-        format.json  { render :json => { :status => 'success' } }
+        format.json  { render :json => { :status => 'error' } }
       end
     end
   end
